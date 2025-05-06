fd2274fd4a1595ff383b5b290e9112871925198d
Raymond <18rj2@queensu.ca>
2025-05-05 21:10:33 -0400

    finished project

--- src/website.py
+++ src/website.py
@@ -26,8 +26,8 @@ def generate_page(from_path, template_path, dest_path, basepath):
     with open(dest_path, "w", encoding="utf-8") as f:
         template = template.replace("{{ Title }}", page_title)
         template = template.replace("{{ Content }}", page_html)
-        template = template.replace("href=\"/", f"href=\"/{basepath}")
-        template = template.replace("src=\"/", f"src=\"/{basepath}")
+        template = template.replace("href=\"/", f"href=\"{basepath}")
+        template = template.replace("src=\"/", f"src=\"{basepath}")
         f.write(template)
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
